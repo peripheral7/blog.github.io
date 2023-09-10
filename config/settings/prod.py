b29970c9ff0d628896949d571b47a27263da135a
Jason Moon <43509390+peripheral7@users.noreply.github.com>
2023-09-10 21:14:50 +0900

    debug false

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,7 +4,7 @@ from .base import *
 ALLOWED_HOSTS = ['15.164.140.211', '*']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []
-DEBUG = True 
+DEBUG = False 
 
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
