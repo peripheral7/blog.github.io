4c96bdde41db70ffb3b9ad70535b1b67932775f6
Moon Chae Geon <mooncg0916@gmail.com>
2024-04-13 17:31:49 +0900

    allowed host changed

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,7 +1,7 @@
 import environ
 from .base import *
 
-ALLOWED_HOSTS = ['15.164.140.211']
+ALLOWED_HOSTS = ['3.35.38.90']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []
 DEBUG = False
