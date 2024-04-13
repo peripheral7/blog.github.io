15ba000cf61f9ced17d9d56b4e3651302c145531
Moon Chae Geon <mooncg0916@gmail.com>
2024-04-13 19:14:49 +0900

    migrate local

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,21 +4,21 @@ from .base import *
 ALLOWED_HOSTS = ['3.35.38.90']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []
-DEBUG = True
-
-env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+DEBUG = False
 #
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': env('DB_NAME'),
-#         'USER': env('DB_USER'),
-#         'PASSWORD': env('DB_PASSWORD'),
-#         'HOST': env('DB_HOST'),
-#         'PORT': '5432',
-#     }
-# }
+# env = environ.Env()
+# environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+#
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': '5432',
+    }
+}
 #
 # SECRET_KEY = env('SECRET_KEY'),
 # SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = env('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
