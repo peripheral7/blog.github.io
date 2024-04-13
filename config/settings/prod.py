9dc59cf6a9b8c519b292b95d621ff25fc42cd672
Moon Chae Geon <mooncg0916@gmail.com>
2024-04-13 17:38:49 +0900

    debug true

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,7 +4,7 @@ from .base import *
 ALLOWED_HOSTS = ['3.35.38.90']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []
-DEBUG = False
+DEBUG = True
 
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
